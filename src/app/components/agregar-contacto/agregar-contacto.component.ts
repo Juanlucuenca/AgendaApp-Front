ba4c07cf66614f7f9e91f4b76aaa5a907c23a760
Juan Lucas Cuenca <70312791+Juanlucuenca@users.noreply.github.com>
2023-11-23 17:15:28 -0300

    modal

--- src/app/components/agregar-contacto/agregar-contacto.component.ts
+++ src/app/components/agregar-contacto/agregar-contacto.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContactsService } from 'src/app/services/contacts.service';
+import { FormsModule } from '@angular/forms';
+import { Contact } from 'src/app/data/interfaces/Contact';
 
 @Component({
-  selector: 'app-agregar-contacto',
+  selector: 'agregar-contacto',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './agregar-contacto.component.html',
   styleUrls: ['./agregar-contacto.component.scss']
 })
 export class AgregarContactoComponent {
+  @Output() cerrar = new EventEmitter();
+  constructor( 
+    public service : ContactsService 
+    ) { }
 
+
+    Contact:Contact = {
+      id: 0,
+      name: '',
+      lastName: '',
+      address: '',
+      email: '',
+      image: '',
+      number: '',
+      company: '',
+      userId: 0
+    }
+
+  public agregarContacto(contacto: Contact) {
+    this.cerrar.emit();
+    this.service.create(contacto);
+  }
+
+  public editarContacto(contacto: Contact) {
+    this.cerrar.emit();
+    this.service.edit(contacto);
+  }
+
+  public closeModal(){
+    this.cerrar.emit();
+  }
 }
