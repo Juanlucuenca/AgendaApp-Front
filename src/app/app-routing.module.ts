2d996e6b3c6bb2429e325ad41387c4f87692eed8
Juan Lucas Cuenca <70312791+Juanlucuenca@users.noreply.github.com>
2023-11-23 16:10:39 -0300

    asd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { usuarioSinLoguear } from './guards/usuario-sin-loguear.guard';
 
 const routes: Routes = [
   {
@@ -9,19 +10,23 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule),
+    // canActivate: [usuarioSinLoguear],
   },
   {
     path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule)
+    loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule),
+    // canActivate: [usuarioSinLoguear],
   },
   {
     path: 'contacts',
-    loadChildren: () => import('./pages/contacts/contacts.module').then(m => m.ContactsModule)
+    loadChildren: () => import('./pages/contacts/contacts.module').then(m => m.ContactsModule),
+    // canActivate: [usuarioSinLoguear],
   },
   {
     path: 'contact',
-    loadChildren: () => import('./pages/detalle-contacto/detalle-contacto.module').then(m => m.DetalleContactoModule)
+    loadChildren: () => import('./pages/detalle-contacto/detalle-contacto.module').then(m => m.DetalleContactoModule),
+    // canActivate: [usuarioSinLoguear],
   },
 ];
 
